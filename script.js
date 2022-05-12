834c92217f3df964df5247b8b22a38bdb10ca27a
AllisonSilveira <allisonsilveiradev@gmail.com>
2022-05-12 16:33:01 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+var content = document.querySelector('div')
+content.style.textAlign = 'center'
+res.style.textAlign = 'center'
+
+function verificar() {
+  var data = new Date()
+  var ano = data.getFullYear()
+  var fano = document.getElementById('txtano')
+  var res = document.querySelector('div#res')
+
+  if (fano.value.length == 0 || Number(fano.value) > ano) {
+    alert('[error] Verifique os dados e tente novamente.')
+  } else {
+    var fsex = document.getElementsByName('radsex')
+    var idade = ano - Number(fano.value)
+    var genero = ''
+    var img = document.createElement('img')
+    img.setAttribute('id', 'foto')
+    if (fsex[0].checked) {
+      genero = 'Homem'
+      if (idade >= 0 && idade < 10) {
+        //criança
+        img.setAttribute('src', 'Assets/menino.png')
+      } else if (idade < 21) {
+        //jovem
+        img.setAttribute('src', 'Assets/jovemhomem.png')
+      } else if (idade < 50) {
+        //adulto
+        img.setAttribute('src', 'Assets/adulto.png')
+      } else {
+        //idoso
+        img.setAttribute('src', 'Assets/idoso.png')
+      }
+    } else if (fsex[1].checked) {
+      genero = 'Mulher'
+      if (idade >= 0 && idade < 10) {
+        //criança
+        img.setAttribute('src', 'Assets/menina.png')
+      } else if (idade < 21) {
+        //jovem
+        img.setAttribute('src', 'Assets/jovemmulher.png')
+      } else if (idade < 50) {
+        //adulto
+        img.setAttribute('src', 'Assets/adulta.png')
+      } else {
+        //idoso
+        img.setAttribute('src', 'Assets/idosa.png')
+      }
+    }
+    res.innerHTML = `detectamos ${genero} com ${idade} anos`
+    res.appendChild(img)
+  }
+}
